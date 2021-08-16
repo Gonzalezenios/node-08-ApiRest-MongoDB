7b9f2ea96db6fc75a239f4eb8d86a4c6994a5d88
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-08-16 18:45:19 -0300

    add jsonwebtoken for authentication

---
+++ src/services/authService.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+const userService = require('./userService');
+const AppError = require('../erros/appError');
+const config = require('../config');
+
+
+const login = async (email, password) => {
+
+    try {
+
+        const user = await userService.findByEmail(email);
+
+        if (!user) {
+            throw new AppError('Authentication failed Email / password does not correct', 400);
+        }
+
+        if (!user.enable) {
+            throw new AppError('Authentication failed / User disabled', 400);
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            throw new AppError('Authentication failed Email / password does not correct.', 400);
+        }
+
+        const token = _encrypt(user._id);
+
+        return {
+            token,
+            user: user.name,
+            role: user.role
+        }
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+_encrypt = (id) => {
+    return jwt.sign({
+        id
+    }, config.auth.secret, { expiresIn: config.auth.ttl });
+}
+
+module.exports = {
+    login
+}
